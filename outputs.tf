5d2c70a8c606ed03b14dc107bf9d8c5f97257656
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-05-06 10:55:32 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -17,7 +17,3 @@ output "alb_security_group_id" {
   value       = module.security.alb_sg_id
 }
 
-output "ecs_security_group_id" {
-  description = "Security Group ID for ECS tasks"
-  value       = module.security.ecs_sg_id
-}

e462be8f95f8484fd81c0abe2dd8bd32b5582e31
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-05-06 10:40:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,20 +16,6 @@ module "iam" {
   app_name  = var.app_name
 }
 
-module "logs" {
-  source    = "./modules/logs"
-  app_name  = var.app_name
-}
-
 module "ecs" {
   source          = "./modules/ecs"
-  app_name        = var.app_name
   cluster_name    = "${var.app_name}-cluster"
-  vpc_id          = module.network.vpc_id
-  subnet_ids      = module.network.public_subnet_ids
-  alb_sg_id       = module.security.alb_sg_id
-  ecs_sg_id       = module.security.ecs_sg_id
-  execution_role  = module.iam.execution_role_arn
-  log_group_name  = module.logs.log_group_name
-  container_port  = var.container_port
-}

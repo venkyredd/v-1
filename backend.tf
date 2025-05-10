01aee73ba7346f53d2d158df5397632dd2fd93ff
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-05-10 23:28:02 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = v-backend-s3
     key            = "infra/terraform.tfstate"
     region         = var.aws_region
-    dynamodb_table = v-backend-db
     encrypt        = true
   }
 }

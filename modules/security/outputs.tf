0c422fc103f834856333667557d790579d8b6ab1
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-05-10 23:39:17 +0530

    Delete modules/security directory

--- modules/security/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "alb_sg_id" {
-  value = aws_security_group.alb.id
-}
-
-output "ecs_sg_id" {
-  value = aws_security_group.ecs.id
-}
--- modules/security/variables.tf
+++
@@ -1,2 +0,0 @@
-variable "vpc_id"            { type = string }
-variable "public_subnet_ids" { type = list(string) }

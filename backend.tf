5239620c952e6f7b9bf4ead2e6805a8df2162406
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-05-12 00:52:52 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = v-backend-s3
-    key            = "infra/terraform.tfstate"
-    region         = var.aws_region
-    encrypt        = true
-  }
-}

7d685dc37bb9f8c498720801b746959568a28da5
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-05-11 10:49:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,8 @@ module "network" {
   vpc_cidr       = var.vpc_cidr
   public_subnets = var.public_subnets
 }
+
+module "security_group" {
+  source = "./modules/security_group"
+  vpc_id = module.network.vpc_id
+}

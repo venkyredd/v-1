1906d831bfd12773952524a0a77144b59d672bb1
root <root@LAPTOP-0FERN5QT.>
2025-05-12 04:13:10 +0000

    Recovered files from commit 5dff59c

--- outputs.tf
+++ outputs.tf
@@ -16,27 +16,3 @@ output "shared_sg_id" {
   value       = module.security_group.shared_sg_id
 }
 
-output "alb_dns_name" {
-  value       = module.alb.alb_dns_name
-  description = "Public DNS of the ALB"
-}
-
-output "alb_target_group_arn" {
-  value       = module.alb.target_group_arn
-  description = "Target group ARN for ECS service"
-}
-
-output "ecs_cluster_id" {
-  value       = module.ecs.ecs_cluster_id
-  description = "The ECS cluster ID"
-}
-
-output "ecr_repository_url" {
-  value       = module.ecs.ecr_repository_url
-  description = "The URL of the ECR repository"
-}
-
-output "ecs_task_execution_role_arn" {
-  value       = module.ecs.ecs_task_execution_role_arn
-  description = "The ARN of the ECS task execution role"
-}

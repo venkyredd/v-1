ae18a03f5ac7d4af1fe330cd3bd5556593502ef0
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-05-12 12:09:35 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -40,3 +40,13 @@ output "ecs_task_execution_role_arn" {
   value       = module.ecs.ecs_task_execution_role_arn
   description = "The ARN of the ECS task execution role"
 }
+
+output "ecs_service_name" {
+  description = "The name of the ECS service"
+  value       = module.ecs_service.ecs_service_name
+}
+
+output "ecs_task_definition_arn" {
+  description = "The ARN of the ECS Task Definition"
+  value       = module.ecs_service.task_definition_arn
+}

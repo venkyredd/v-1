842ab2aa28eb2fa762e4298a995926352b8563bb
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-05-12 10:28:50 +0530

    Update variables.tf

--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -1,3 +1,15 @@
+variable "cluster_name" {
+  description = "The name of the ECS cluster"
+  type        = string
+  default     = "my-ecs-cluster"
+}
+
+variable "repository_name" {
+  description = "The name of the ECR repository"
+  type        = string
+  default     = "my-ecr-repo"
+}
+
 # IAM Role for ECS Tasks
 variable "ecs_task_execution_role_arn" {
   description = "The ARN of the ECS task execution role"
